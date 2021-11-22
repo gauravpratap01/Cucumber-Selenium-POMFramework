8e596d9fd7c1550c41e3c5788c3a52a3d62954f7
gauravpratap01 <engg.gaurav89@gmail.com>
2021-11-22 15:30:19 +0800

    First Commit for FW

---
+++ src/main/java/com/pages/LoginPage.java
@@ -0,0 +1,40 @@
+package com.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+	
+	private WebDriver driver;
+	
+	// Define By locators first
+	private By emailId=By.id("email");
+	private By password=By.id("passwd");
+	private By signInButton=By.id("SubmitLogin");
+	private By forgotPswdLink=By.linkText("Forgot your password?");
+	
+	// Define constructor
+	public LoginPage(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+	//Define public page actions
+	public String getLoginPageTitle() {
+		return driver.getTitle();
+	}
+	
+	public boolean isForgotPasswdLinkExist() {
+		return driver.findElement(forgotPswdLink).isDisplayed();
+	}
+	
+	public void enterUserName(String username) {
+		driver.findElement(emailId).sendKeys(username);
+	}
+	public void enterPassword(String pwd) {
+		driver.findElement(password).sendKeys(pwd);
+	}
+	public void clickOnLogin() {
+		driver.findElement(signInButton).click();
+	}
+
+}
