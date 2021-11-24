4d10d1549956ff6c7687b6119f01b9efe307d613
gauravpratap01 <engg.gaurav89@gmail.com>
2021-11-25 01:20:29 +0800

    added YAML  file

--- src/test/java/testRunners/MyTestRunner.java
+++ src/test/java/testRunners/MyTestRunner.java
@@ -7,7 +7,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features = {"./src/test/resources/appFeatures"},
+		features = {"./src/test/resources/appFeatures/AccountsPage.feature"},
 		glue = {"stepDefinitions","AppHooks"},
 		plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 		monochrome = true
